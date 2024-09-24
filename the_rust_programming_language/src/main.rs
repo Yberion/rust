0a4b9817f1f41531b548e7c5ad8de496065f9399
Yberion <Brandon.Largeau@gmail.com>
2024-09-24 08:55:07 +0200

    feat(the_rust_programming_language): WIP - Building a Command Line Program

--- the_rust_programming_language/src/main.rs
+++ the_rust_programming_language/src/main.rs
@@ -1,7 +1,9 @@
 mod _2_programming_a_guessing_game;
 mod _8_common_collections;
+mod _12_building_a_command_line_program;
 
 fn main() {
     //_2_programming_a_guessing_game::main::test();
-    _8_common_collections::main::test();
+    //_8_common_collections::main::test();
+    _12_building_a_command_line_program::main::test();
 }
